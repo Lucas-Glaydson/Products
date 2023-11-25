fd50c7813ae8d1aa229cd82008f1846d0be8066f
lucas <lucas.glaydson@jala.university>
2023-11-25 00:17:00 -0300

    release 1.0.0

--- src/main/java/com/products/products/controllers/ProductController.java
+++ src/main/java/com/products/products/controllers/ProductController.java
@@ -5,7 +5,6 @@ import com.products.products.models.ProductModel;
 import com.products.products.repository.ProductRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
